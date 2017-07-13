bac19c5e3577419441423fac1b45cc1175f42828
Boris Khakhin <jentix@yandex.ru>
2017-07-13 16:55:16 +0300

    update gitignore

---
+++ parties-map/DataStore.swift
@@ -0,0 +1,13 @@
+//
+//  DataStore.swift
+//  parties-map
+//
+//  Created by Jentix on 13.07.17.
+//  Copyright © 2017 Jentix. All rights reserved.
+//
+
+import Foundation
+
+class DataStore {
+    
+}
